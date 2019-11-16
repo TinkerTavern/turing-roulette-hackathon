f53a5553c8e5d368f6b8c7dd4eca667edcb95de7
Tom Lancaster <43844772+TinkerTavern@users.noreply.github.com>
2019-11-16 14:53:45 +0000

    Added some Basic Endpoints

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
+def index():
+    return 'Welcome to the website'
+
+@app.route('/chat')
+def chat():
+    return 'Connecting you to a \'user\'..'
+
+@app.route('/')
 def hello_world():
     return 'Hello, World!'
 
